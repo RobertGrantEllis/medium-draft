8fcedf3d3de910461f615ecd5ffb3a273c8c9616
Robert Ellis <robert.grant.ellis@gmail.com>
2018-07-20 17:11:01 -0400

    fix addNewBlock function so that it actually adds a new block if the current one is nonempty

--- src/model/index.js
+++ src/model/index.js
@@ -25,9 +25,62 @@ export const getCurrentBlock = (editorState) => {
   return block;
 };
 
+// const BEFORE = -1;
+// const AFTER = 1;
+
 /*
-Adds a new block (currently replaces an empty block) at the current cursor position
-of the given `newType`.
+Used from [react-rte](https://github.com/sstur/react-rte/blob/master/src/lib/insertBlockAfter.js)
+by [sstur](https://github.com/sstur)
+*/
+export const addNewBlockAt = (
+  editorState,
+  pivotBlockKey,
+  newBlockType = Block.UNSTYLED,
+  initialData = {}
+) => {
+  const content = editorState.getCurrentContent();
+  const blockMap = content.getBlockMap();
+  const block = blockMap.get(pivotBlockKey);
+  if (!block) {
+    throw new Error(`The pivot key - ${pivotBlockKey} is not present in blockMap.`);
+  }
+  const blocksBefore = blockMap.toSeq().takeUntil((v) => (v === block));
+  const blocksAfter = blockMap.toSeq().skipUntil((v) => (v === block)).rest();
+  const newBlockKey = genKey();
+
+  const newBlock = new ContentBlock({
+    key: newBlockKey,
+    type: newBlockType,
+    text: '',
+    characterList: List(),
+    depth: 0,
+    data: Map(getDefaultBlockData(newBlockType, initialData)),
+  });
+
+  const newBlockMap = blocksBefore.concat(
+    [[pivotBlockKey, block], [newBlockKey, newBlock]],
+    blocksAfter
+  ).toOrderedMap();
+
+  const selection = editorState.getSelection();
+
+  const newContent = content.merge({
+    blockMap: newBlockMap,
+    selectionBefore: selection,
+    selectionAfter: selection.merge({
+      anchorKey: newBlockKey,
+      anchorOffset: 0,
+      focusKey: newBlockKey,
+      focusOffset: 0,
+      isBackward: false,
+    }),
+  });
+  return EditorState.push(editorState, newContent, 'split-block');
+};
+
+/*
+Adds a new block at the current cursor position with the designated `newType` and `initialData`.
+If the current block is empty, replaces the current empty block instead.
 */
 export const addNewBlock = (editorState, newType = Block.UNSTYLED, initialData = {}) => {
   const selectionState = editorState.getSelection();
@@ -41,21 +94,21 @@ export const addNewBlock = (editorState, newType = Block.UNSTYLED, initialData =
   if (!currentBlock) {
     return editorState;
   }
-  if (currentBlock.getLength() === 0) {
-    if (currentBlock.getType() === newType) {
-      return editorState;
-    }
-    const newBlock = currentBlock.merge({
-      type: newType,
-      data: getDefaultBlockData(newType, initialData),
-    });
-    const newContentState = contentState.merge({
-      blockMap: blockMap.set(key, newBlock),
-      selectionAfter: selectionState,
-    });
-    return EditorState.push(editorState, newContentState, 'change-block-type');
+  if (currentBlock.getLength() > 0) {
+    // need a brand-new block
+    return addNewBlockAt(editorState, key, newType, initialData);
   }
-  return editorState;
+
+  // leverage existing empty block
+  const newBlock = currentBlock.merge({
+    type: newType,
+    data: getDefaultBlockData(newType, initialData),
+  });
+  const newContentState = contentState.merge({
+    blockMap: blockMap.set(key, newBlock),
+    selectionAfter: selectionState,
+  });
+  return EditorState.push(editorState, newContentState, 'change-block-type');
 };
 
 
@@ -95,59 +148,6 @@ export const updateDataOfBlock = (editorState, block, newData) => {
     blockMap: contentState.getBlockMap().set(block.getKey(), newBlock),
   });
   return EditorState.push(editorState, newContentState, 'change-block-data');
-};
-
-// const BEFORE = -1;
-// const AFTER = 1;
-
-/*
-Used from [react-rte](https://github.com/sstur/react-rte/blob/master/src/lib/insertBlockAfter.js)
-by [sstur](https://github.com/sstur)
-*/
-export const addNewBlockAt = (
-    editorState,
-    pivotBlockKey,
-    newBlockType = Block.UNSTYLED,
-    initialData = {}
-  ) => {
-  const content = editorState.getCurrentContent();
-  const blockMap = content.getBlockMap();
-  const block = blockMap.get(pivotBlockKey);
-  if (!block) {
-    throw new Error(`The pivot key - ${pivotBlockKey} is not present in blockMap.`);
-  }
-  const blocksBefore = blockMap.toSeq().takeUntil((v) => (v === block));
-  const blocksAfter = blockMap.toSeq().skipUntil((v) => (v === block)).rest();
-  const newBlockKey = genKey();
-
-  const newBlock = new ContentBlock({
-    key: newBlockKey,
-    type: newBlockType,
-    text: '',
-    characterList: List(),
-    depth: 0,
-    data: Map(getDefaultBlockData(newBlockType, initialData)),
-  });
-
-  const newBlockMap = blocksBefore.concat(
-    [[pivotBlockKey, block], [newBlockKey, newBlock]],
-    blocksAfter
-  ).toOrderedMap();
-
-  const selection = editorState.getSelection();
-
-  const newContent = content.merge({
-    blockMap: newBlockMap,
-    selectionBefore: selection,
-    selectionAfter: selection.merge({
-      anchorKey: newBlockKey,
-      anchorOffset: 0,
-      focusKey: newBlockKey,
-      focusOffset: 0,
-      isBackward: false,
-    }),
-  });
-  return EditorState.push(editorState, newContent, 'split-block');
 };
 
 /**
